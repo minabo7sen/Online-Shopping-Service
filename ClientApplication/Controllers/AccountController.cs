575ad5820fca3bc38dc59dc6ce2cf7da8847cb15
Mina Mohsen <minamhakim@gmail.com>
2019-12-17 13:29:45 +0200

    Added Missing Registeration properties

--- ClientApplication/Controllers/AccountController.cs
+++ ClientApplication/Controllers/AccountController.cs
@@ -151,7 +151,7 @@ namespace ClientApplication.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Username, Email = model.Email, PhoneNumber = model.Phone };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
