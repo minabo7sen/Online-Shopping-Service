7f14567ea7e6cf0b86f5a1cae7e39a8458d89985
Mina Mohsen <minamhakim@gmail.com>
2019-12-17 14:19:14 +0200

    Fixed Login Functionality

--- ClientApplication/Models/AccountViewModels.cs
+++ ClientApplication/Models/AccountViewModels.cs
@@ -49,9 +49,13 @@ namespace ClientApplication.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        //[Required]
+        //[Display(Name = "Email")]
+        //[EmailAddress]
+        //public string Email { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
